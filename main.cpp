2db4909ad4c16cb398292e40ff323551ce3dbfa8
Adam <admgrac@gmail.com>
2023-08-16 13:48:29 +0200

    add first test in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
 #include "Sieve.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
 
+	Sieve sieve(1000UL);
+	if (sieve.savePrimesToFile("primesUpTo1000.txt"))
+	{
+		cout << "Success" << endl;
+	}
+	else {
+		cout << "Failure" << endl;
+	}
 
 }
