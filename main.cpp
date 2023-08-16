1a1bc98a615743a6498d6632be17bdb41869e713
Adam <admgrac@gmail.com>
2023-08-16 13:51:29 +0200

    add Sieve class exemplary usage in main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include "Sieve.h"
-#include <iostream>
-
 using namespace std;
 
 int main() {
+	// Sieve class exemplary usage:
 
-	/*Sieve sieve(1000UL);
-	if (sieve.savePrimesToFile("primesUpTo1000.txt"))
-	{
-		cout << "Success" << endl;
+	// constructor:
+	int endingNumber{ 1000 };
+	Sieve<int> sieve(endingNumber);
+
+	cout << "There are " << sieve.countPrimes() << " prime numbers from 1 to " << endingNumber << "." << endl;
+
+	// to extract primes from the object use getPrimes:
+	auto primes = sieve.getPrimes(); // std::list<int> int this example
+
+	// to save the result in a file use saveToFile:
+	string filename("primes.txt");
+	if (sieve.savePrimesToFile(filename)) {
+		cout << "The file was saved successfully." << endl;
 	}
 	else {
-		cout << "Failure" << endl;
-	}*/
+		cout << "Error while saving to a file..." << endl;
+	}
 
+	// to print the primes to stdout use printPrimes with std::cout or operator<<
+	//sieve.printPrimes(cout, "\n"); // beware that this would print all the primes to stdout :)
+
+	system("pause");
 }
